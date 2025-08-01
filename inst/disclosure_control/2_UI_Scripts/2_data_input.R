653209e4a9ef871d9b7329dccd4a63a62bb24243
rjmithcell92 <robertjames.mitchell@phs.scot>
2025-08-01 14:39:17 +0000

    Streamline code for ui for main data input tab.

--- inst/disclosure_control/2_UI_Scripts/2_data_input.R
+++ inst/disclosure_control/2_UI_Scripts/2_data_input.R
@@ -1,34 +1,30 @@
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-#Run on Posit version: 4.1.2
-#Last updated: 02 Oct 2024
-#By: Robert Mitchell
-#Script: 2_data_input.R
-#Purpose: Set up UI for the Main Tab used for uploading/processing input data
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+# ------------------------------------------------------------------------------
+# Script Name : 2_data_input.R
+# Purpose     : Set up UI for the Main Tab used for uploading/processing input data
+# Last Update : 01 Aug 2025
+# Author      : Robert Mitchell
+# Posit Version: 4.4.2
+# ------------------------------------------------------------------------------
 
-# Create 1. Input Data Main Tab
-shiny::tabPanel("1. Input Data", shiny::navlistPanel(
-  id = "tabset",
-  widths = sb_width,
+# Input Data Main Tab
+shiny::tabPanel(
+  "1. Input Data",
+  shiny::navlistPanel(
+    id = "tabset",
+    widths = sb_width,
 
-  # 1. Data Input Choice Section ----
-  source("2_UI_Scripts/2-01_dummy_data_choice.R",  local = TRUE)$value,
+    # 1. Dummy Data Input Choice
+    source("2_UI_Scripts/2-01_dummy_data_choice.R", local = TRUE)$value,
 
-  # 2. File Upload Section ----
-  source("2_UI_Scripts/2-02_file_upload.R",  local = TRUE)$value,
+    # 2. File Upload
+    source("2_UI_Scripts/2-02_file_upload.R", local = TRUE)$value,
 
-  # 3. Data Process Section ----
-  source("2_UI_Scripts/2-03_var_convert.R",  local = TRUE)$value,
+    # 3. Data Processing
+    source("2_UI_Scripts/2-03_var_convert.R", local = TRUE)$value,
 
-  # 4. App Data Summary Section ----
-  source("2_UI_Scripts/2-04_post_proc_summary.R",  local = TRUE)$value
+    # 4. Post-Processing Summary
+    source("2_UI_Scripts/2-04_post_proc_summary.R", local = TRUE)$value
   )
-  )
+)
 
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-# End ----
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+# END OF SCRIPT ----
