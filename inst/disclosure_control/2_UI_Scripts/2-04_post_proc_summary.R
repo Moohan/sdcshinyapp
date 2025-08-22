51b5776d223ddde5f7b6d0b287c745d93216f9bc
rjmithcell92 <robertjames.mitchell@phs.scot>
2025-08-22 07:09:11 +0000

    2-04_post_proc_summary.R Streamine this code for improved readability and maintenance.

--- inst/disclosure_control/2_UI_Scripts/2-04_post_proc_summary.R
+++ inst/disclosure_control/2_UI_Scripts/2-04_post_proc_summary.R
@@ -1,34 +1,25 @@
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-#Run on Posit version: 4.1.2
-#Last updated: 21 Feb 2025
-#By: Robert Mitchell
-#Script: 2-04_post_proc_summary.R
-#Purpose: Set up UI for the Sub-Tab used to visualise Data Summary after processing
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+# ------------------------------------------------------------------------------
+# Script Name : 2-04_post_proc_summary.R
+# Purpose     : UI for Sub-Tab to visualise Data Summary after processing
+# Last Update : 22 Aug 2025
+# Author      : Robert Mitchell
+# Posit Version: 4.4.2
+# ------------------------------------------------------------------------------
 
-# Create Data Summary after processing Sub Tab for 1. Input Data Main Tab
-shiny::tabPanel("Data Summary after processing",
-
-                # Input and Output Definitions
-                shiny::fluidRow(
-
-                  # Output Definitions
-                  shiny::mainPanel(
-                    shiny::h1(shiny::strong("Data Information")), # Main Section Title
-                    shiny::br(), # New Line
-                    shiny::h2(shiny::strong("Data Summary")), # Sub Heading for Data Summary
-                    shiny::verbatimTextOutput("process_summary_dist"), # Visualisation of Processed Data in App
-                    shiny::br(),  # New Line
-                    shiny::h2(shiny::strong("Percentage of Missing Values for each Variable")), # Sub Heading for Missing Data Summary
-                    shiny::verbatimTextOutput("process_missing") # Visualisation of Missing Data in App
-                  )
-                )
+shiny::tabPanel(
+  "Data Summary after processing",
+  shiny::fluidRow(
+    # Output Panel
+    shiny::mainPanel(
+      shiny::h1(shiny::strong("Data Information")),
+      shiny::br(),
+      shiny::h2(shiny::strong("Data Summary")),
+      shiny::verbatimTextOutput("process_summary_dist"),
+      shiny::br(),
+      shiny::h2(shiny::strong("Percentage of Missing Values for each Variable")),
+      shiny::verbatimTextOutput("process_missing")
+    )
+  )
 )
 
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-# End ----
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+# END OF SCRIPT ----
