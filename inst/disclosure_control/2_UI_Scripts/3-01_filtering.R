f1b58612fac937068ed5632a70a69da4d32e7e48
rjmithcell92 <robertjames.mitchell@phs.scot>
2025-08-22 07:20:44 +0000

    3-01_filtering.R & 3-02_formatting.R Streamine code in these UI scripts for improved readability and maintenance.

--- inst/disclosure_control/2_UI_Scripts/3-01_filtering.R
+++ inst/disclosure_control/2_UI_Scripts/3-01_filtering.R
@@ -1,43 +1,40 @@
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-#Run on Posit version: 4.1.2
-#Last updated: 01 Oct 2024
-#By: Robert Mitchell
-#Script: 3-01_filtering.R
-#Purpose: To set up UI for the Filtering Sub Tab
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+# ------------------------------------------------------------------------------
+# Script Name : 3-01_filtering.R
+# Purpose     : UI for Filtering Sub-Tab
+# Last Update : 22 Aug 2025
+# Author      : Robert Mitchell
+# Posit Version: 4.4.2
+# ------------------------------------------------------------------------------
 
-# Create Filtering Sub-Tab for 2. Input Data Filtering/Formatting Tab
-shiny::tabPanel("Filtering",
+# Filtering Sub-Tab for 2. Input Data Filtering/Formatting Tab
+shiny::tabPanel(
+  "Filtering",
+  shiny::fluidRow(
+    # Input Definitions
+    shiny::sidebarPanel(
+      shiny::h5(shiny::strong("Store Unprocessed Data")),
+      shiny::actionButton("Unprocess_Store", "Store"),
+      shiny::br(),
 
-         #Input and output Definitions
-         shiny::fluidRow(
+      shiny::h5(shiny::strong("Filter Data")),
+      shiny::actionButton("addFilter", "Add filter"),
+      shiny::div(id = "placeholderAddRemFilt"),
+      shiny::div(id = "placeholderFilter"),
+      shiny::br(),
 
-           # Input Definitions
-           shiny::sidebarPanel(
-             shiny::h5(shiny::strong("Store Unprocessed Data")), # Header to indicate Button for data storage
-             shiny::actionButton('Unprocess_Store', 'Store'), # Button to store data prior to filtering
-             shiny::br(), # New Line
-             shiny::h5(shiny::strong("Filter Data")), # Header to indicate Filter Button
-             shiny::actionButton('addFilter', 'Add filter'), # Button to filter Data
-             shiny::div(id = 'placeholderAddRemFilt'),
-             shiny::div(id = 'placeholderFilter'), # Used to insert UI for Add Filter options
-             shiny::br(), # New Line
-             shiny::h5(shiny::strong("Store Filtered Data")),
-             shiny::actionButton('store_data', 'Store'), #Button to store filtered data
-             shiny::br(), # New Line
-             shiny::h5(strong("Re-add Filtered Data Back In")),
-             shiny::actionButton('re_add_data', 'Re-add') #Button to re-add filtered data back into data frame
-             ),
+      shiny::h5(shiny::strong("Store Filtered Data")),
+      shiny::actionButton("store_data", "Store"),
+      shiny::br(),
 
-           # Output Definitions
-           shiny::mainPanel(DT::dataTableOutput("filtered_data")) # Visualisation of Filtered Data in App
-           )
-         )
+      shiny::h5(shiny::strong("Re-add Filtered Data Back In")),
+      shiny::actionButton("re_add_data", "Re-add")
+    ),
 
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-# End ----
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+    # Output Definitions
+    shiny::mainPanel(
+      DT::dataTableOutput("filtered_data")
+    )
+  )
+)
+
+# END OF SCRIPT ----
--- inst/disclosure_control/2_UI_Scripts/3-02_formatting.R
+++ inst/disclosure_control/2_UI_Scripts/3-02_formatting.R
@@ -1,50 +1,50 @@
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-#Run on Posit version: 4.1.2
-#Last updated: 01 Oct 2024
-#By: Robert Mitchell
-#Script: 3-02_formatting.R
-#Purpose: To set up UI for the Data Formatting Sub Tab
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+# ------------------------------------------------------------------------------
+# Script Name : 3-02_formatting.R
+# Purpose     : UI for Data Formatting Sub-Tab
+# Last Update : 22 Aug 2025
+# Author      : Robert Mitchell
+# Posit Version: 4.4.2
+# ------------------------------------------------------------------------------
 
-# Create Transform Format Sub-Tab for 2. Input Data Filtering/Formatting Tab
-shiny::tabPanel("Transform Format",
+# Transform Format Sub-Tab for 2. Input Data Filtering/Formatting Tab
+shiny::tabPanel(
+  "Transform Format",
+  shiny::fluidRow(
+    # Input Definitions
+    shiny::sidebarPanel(
+      shiny::h4(shiny::strong("Long to Wide Data Transformation")),
+      shiny::h5(shiny::strong("Remove Serial Number")),
+      shiny::actionButton("rem_ser", "Remove"),
+      shiny::br(),
 
-                #Input and output Definitions
-                shiny::fluidRow(
+      shiny::h5(shiny::strong("Re-add Serial Number")),
+      shiny::actionButton("re_add_ser", "Serial"),
+      shiny::br(),
 
-                  # Input Definitions
-                  shiny::sidebarPanel(
-                    shiny::h4(shiny::strong("Long to Wide Data Transformation")), # Header for long to wide data transformation section
-                    shiny::h5(shiny::strong("Remove Serial Number")), # Header to indicate Button for serial number removal
-                    shiny::actionButton('rem_ser', 'Remove'), #Button to remove serial number
-                    shiny::br(), # New Line
-                    shiny::h5(shiny::strong("Re-add Serial Number")), # Header to indicate Button for serial number being re-added
-                    shiny::actionButton('re_add_ser', 'Serial'), # Button to put Serial number back
-                    shiny::br(), # New Line
-                    shiny::selectInput("keyvariableId", label = "Choose Key Variable", choices = ""), # Key Variable for long to wide transform
-                    shiny::selectInput("valuevariableId", label = "Choose Value Variable", choices = ""), # Value Variable for long to wide transform
-                    shiny::h5(shiny::strong("Transform Long Data into Wide Data")), # Header to indicate Button for data transformation
-                    shiny::actionButton('long_wide_trans', 'Transform'), #Button to transform long long into wide data
-                    shiny::h4(shiny::strong("Wide to Long Data Transformation")), # Header for wide to wide data transformation section
-                    shiny::h5(shiny::strong("Transform Wide Data into Long Data")), # Header to indicate Button for data transformation
-                    shiny::actionButton('wide_long_trans', 'Transform'), #Button to transform wide data back into long data
-                    shiny::br(), # New Line
-                    shiny::br(), # New Line
-                    shiny::br(), # New Line
-                    shiny::h4("Transform Information"), # Header to indicate help text
-                    shiny::p("For the long to wide data transform, if the wrong variables are entered in the data transform stage, go to ", shiny::strong("Tab 5"),
-                             "and click the Reset data button. Also, for this stage, the serial number must be removed.") # Help Text
-                    ),
+      shiny::selectInput("keyvariableId", "Choose Key Variable", choices = ""),
+      shiny::selectInput("valuevariableId", "Choose Value Variable", choices = ""),
 
-                  # Output Definitions
-                  shiny::mainPanel(DT::dataTableOutput("format_data")) # Visualisation of Formatted Data in App
-                  )
-                )
+      shiny::h5(shiny::strong("Transform Long Data into Wide Data")),
+      shiny::actionButton("long_wide_trans", "Transform"),
 
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-# End ----
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+      shiny::h4(shiny::strong("Wide to Long Data Transformation")),
+      shiny::h5(shiny::strong("Transform Wide Data into Long Data")),
+      shiny::actionButton("wide_long_trans", "Transform"),
+      shiny::br(), shiny::br(), shiny::br(),
+
+      shiny::h4("Transform Information"),
+      shiny::p(
+        "For the long to wide data transform, if the wrong variables are entered, go to ",
+        shiny::strong("Tab 5"),
+        " and click the Reset data button. Also, the serial number must be removed before this step."
+      )
+    ),
+
+    # Output Definitions
+    shiny::mainPanel(
+      DT::dataTableOutput("format_data")
+    )
+  )
+)
+
+# END OF SCRIPT ----
