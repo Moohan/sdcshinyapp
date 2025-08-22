9d3f36ca8f83017a1ac083cffd4da15490dcdcae
rjmithcell92 <robertjames.mitchell@phs.scot>
2025-08-22 08:22:39 +0000

    3-02_formatting.R
    Streamine code for data format transform for improved readability and maintenance.

--- inst/disclosure_control/3_Server_Scripts/3-02_formatting.R
+++ inst/disclosure_control/3_Server_Scripts/3-02_formatting.R
@@ -1,293 +1,117 @@
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-#Run on Posit version: 4.1.2
-#Last updated: 02 Oct 2024
-#By: Robert Mitchell
-#Script: 3-02_formatting.R
-#Purpose: To transform data format
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+# ------------------------------------------------------------------------------
+# Script Name : 3-02_formatting.R
+# Purpose     : Transform data format (long ↔ wide)
+# Last Update : 22 Aug 2025
+# Author      : Robert Mitchell
+# Posit Version: 4.4.2
+# ------------------------------------------------------------------------------
 
 # 1. Serial Number Removal ----
+Serial_Removed <- shiny::reactiveValues(data = NULL)
 
-# Dataframe for removed Serials
-Serial_Removed <- shiny::reactiveValues(data=NULL)
-
-## 1. Remove with button press ----
 shiny::observeEvent(input$rem_ser, {
-
-  # Stop if Serial variable in Data
-  if ("Serial" %notin% colnames(App_data$values)){
-
-    # Error Notification
-    shinyalert::shinyalert(title = "No serial number attached to data.",
-                           text = "No serial number to remove.",
-                           type = "error")
-
-    # Validation
+  if (!"Serial" %in% colnames(App_data$values)) {
+    shinyalert::shinyalert("No serial number attached to data.", "No serial number to remove.", type = "error")
     shiny::validate(shiny::need(App_data$values$Serial, "There is no Serial Number"))
+  }
 
-    }
-
-  # Notification saying that Serial Number has been removed
-  shinyalert::shinyalert(title = "Serial number removed", type = "success")
-
-  #Temp Storage of data prior to Serial Removal
-  shiny::isolate({temp <- App_data$values})
-
-  # Store Serial Numbers
-  Serial_Removed$data <- temp |>
-    dplyr::select(Serial)
-
-  # Remove Serial Number
-  App_data$values <- temp |>
-    dplyr::select(-Serial)
-
-  #Clear Temp Storage
-  temp <- NULL
-
-  })
-
-# 2. Re-add Serial ----
-
-## 1. Re-add Serial ----
-shiny::observeEvent(input$re_add_ser, {
-
-  # Stop if Serial is present
-  if (is.null(Serial_Removed$data)){
-
-    # Error Notification
-    shinyalert::shinyalert(title = "Serial number has not been removed.",
-                           type = "error")
-
-    # Validation
-    shiny::validate(shiny::need(Serial_Removed$data, "There is no Serial Number that has been removed"))
-
-    }
-
-  # Stop if no. of rows for data doen't match no. of Serials
-  if (nrow(App_data$values) != nrow(Serial_Removed$data)){
-
-    # Error Notification
-    shinyalert::shinyalert(title = "Number of observations in datasets different to the number of removed serial numbers.",
-                           text = "Please ensure that numbers of observation in dataset is the same as number of serial numbers.",
-                           type = "error")
-
-    # Validation
-      shiny::validate(shiny::need(nrow(App_data$values) == nrow(Serial_Removed$data),
-                                  "The stored serial numbers have a different number of rows to data frame"))
-
-      }
-
-  #Notification indicating that Serial number has been re-added
-  shinyalert::shinyalert(title = "Serial number successfully re-added.",
-                         type = "success")
-
-  # Re-add Serial number
-  App_data$values <- App_data$values|>
-    cbind(Serial_Removed$data) |>
-    dplyr::select(Serial,dplyr::everything())
-
-  # Clear Stored Serial Number
-  Serial_Removed$data <- NULL
-
-  })
-
-# 3. Update Selectboxes ----
-# Updates key and value variable select boxes prior to data transformation
-
-## 1. Update select input box with variable names for key variable ----
-shiny::observe({
-
-  # Update options
-  cb_options <- sdcshinyapp::SelectBox_Update(App_data$values)
-
-  # Update Selectbox
-  shiny::updateSelectInput(session, "keyvariableId",
-                           label = "Choose Key Variable",
-                           choices = cb_options,
-                           selected = "")
-  })
-
-
-## 2. Update select input box with variable names for value variable ----
-shiny::observe({
-
-  # Update options
-  cb_options <- sdcshinyapp::SelectBox_Update(App_data$values)
-
-  # Update Selectbox
-  shiny::updateSelectInput(session, "valuevariableId",
-                           label = "Choose Value Variable",
-                           choices = cb_options,
-                           selected = "")
-
-  })
-
-
-# 4. Store Variables ----
-# This stores variables prior to data transformation
-
-## 1. Key Variable Details ----
-
-key_value_options <- shiny::reactiveValues(data=NULL) # Options
-key_value_header <- shiny::reactiveValues(header=NULL) # Variable Name
-
-## 2. Value Variable Details ----
-
-variable_value_header <- shiny::reactiveValues(header=NULL) # Variable Name
-
-## 3. Column Names in original order ----
-
-column_values <- shiny::reactiveValues(order=NULL)
-
-
-# 5. Long to Wide Transformation ----
-
-## 1. Data Transformation ----
-shiny::observeEvent(input$long_wide_trans, {
-
-  # Stop if key & value variable not given
-    if (input$keyvariableId == "" & input$valuevariableId == ""){
-
-      # Error Notification
-      shinyalert::shinyalert(title = "There is no key or value variable selected",
-                             text = "Please select key variable and value variable.",
-                             type = "error")
-
-      # Validation
-      shiny::validate(shiny::need(input$keyvariableId , "There is no key variable"),
-                      shiny::need(input$valuevariableId , "There is no value variable"))
-
-      # Stop if value variable not given
-    } else if (input$keyvariableId != "" & input$valuevariableId == ""){
-
-      # Error Notification
-      shinyalert::shinyalert(title = "There is no value variable selected",
-                             text = "Please select value variable.",
-                             type = "error")
-
-      # Validation
-      shiny::validate(shiny::need(input$valuevariableId , "There is no value variable"))
-
-      # Stop if key variable not given
-      } else if (input$keyvariableId == "" & input$valuevariableId != "") {
-
-        # Error Notification
-        shinyalert::shinyalert(title = "There is no key variable selected",
-                               text = "Please select key variable.",
-                               type = "error")
-
-        # Validation
-        shiny::validate(shiny::need(input$keyvariableId , "There is no key variable"))
-
-      }
-
-  #Un-formatted Data Storage
-  shiny::isolate({temp_format <- App_data$values})
-
-  # Key Variable
-  key_value_header$header <- input$keyvariableId
-
-  # Value Variable
-  variable_value_header$header <- input$valuevariableId
-
-  # Key Value Options
-  key_value_options$data <- unique(temp_format[,input$keyvariableId])
-  key_value_options$data <- data.frame(key_value_options$data)
-  names(key_value_options$data)[names(key_value_options$data) == "orig_var"] <- input$keyvariableId
-  key_value_options$data  <- key_value_options$data |>
-    dplyr::pull()
-
-  # Column Header - Used to put data frame columns back to original order for transforming back to long data
-  column_values$order <- colnames(temp_format)
-
-  # Transforms long to wide - generates error notification if transformation is unsuccessful
-  tryCatch(
-
-    # Checks for error - if no error, this then performs the transformation
-    expr = {App_data$values <- temp_format |>
-      tidyr::spread(input$keyvariableId,input$valuevariableId)
-
-    # Success Notification
-    shinyalert::shinyalert(title = "Long to wide transformation successful",
-                           type = "success")},
-
-    # Error Notification
-    error = function(e)
-      shinyalert::shinyalert(title = "Transformation Unsuccessful. Please reset data.",
-                             type = "error")
-    )
-
-  # Clear Unformatted data
-  temp_format <- NULL
-
+  shinyalert::shinyalert("Serial number removed", type = "success")
+  temp <- shiny::isolate(App_data$values)
+  Serial_Removed$data <- temp |> dplyr::select(Serial)
+  App_data$values <- temp |> dplyr::select(-Serial)
 })
 
-# 6. Wide to Long Transform ----
-# Only used when Long to Wide Transformation has occured
+# 2. Re-add Serial Number ----
+shiny::observeEvent(input$re_add_ser, {
+  if (is.null(Serial_Removed$data)) {
+    shinyalert::shinyalert("Serial number has not been removed.", type = "error")
+    shiny::validate(shiny::need(Serial_Removed$data, "No Serial Number to re-add"))
+  }
 
-## 1. Transform data ----
+  if (nrow(App_data$values) != nrow(Serial_Removed$data)) {
+    shinyalert::shinyalert("Mismatch in row counts", "Ensure row counts match before re-adding Serial.", type = "error")
+    shiny::validate(shiny::need(nrow(App_data$values) == nrow(Serial_Removed$data), "Row count mismatch"))
+  }
+
+  shinyalert::shinyalert("Serial number successfully re-added.", type = "success")
+  App_data$values <- cbind(Serial_Removed$data, App_data$values) |> dplyr::select(Serial, dplyr::everything())
+  Serial_Removed$data <- NULL
+})
+
+# 3. Update Select Inputs ----
+shiny::observe({
+  cb_options <- sdcshinyapp::SelectBox_Update(App_data$values)
+  shiny::updateSelectInput(session, "keyvariableId", "Choose Key Variable", choices = cb_options, selected = "")
+})
+
+shiny::observe({
+  cb_options <- sdcshinyapp::SelectBox_Update(App_data$values)
+  shiny::updateSelectInput(session, "valuevariableId", "Choose Value Variable", choices = cb_options, selected = "")
+})
+
+# 4. Store Variables ----
+key_value_options <- shiny::reactiveValues(data = NULL)
+key_value_header <- shiny::reactiveValues(header = NULL)
+variable_value_header <- shiny::reactiveValues(header = NULL)
+column_values <- shiny::reactiveValues(order = NULL)
+
+# 5. Long to Wide Transformation ----
+shiny::observeEvent(input$long_wide_trans, {
+  if (input$keyvariableId == "" || input$valuevariableId == "") {
+    shinyalert::shinyalert("Missing selection", "Please select both key and value variables.", type = "error")
+    shiny::validate(
+      shiny::need(input$keyvariableId, "No key variable"),
+      shiny::need(input$valuevariableId, "No value variable")
+    )
+  }
+
+  temp_format <- shiny::isolate(App_data$values)
+  key_value_header$header <- input$keyvariableId
+  variable_value_header$header <- input$valuevariableId
+
+  key_value_options$data <- unique(temp_format[[input$keyvariableId]]) |> as.character()
+  column_values$order <- colnames(temp_format)
+
+  tryCatch({
+    App_data$values <- temp_format |> tidyr::spread(input$keyvariableId, input$valuevariableId)
+    shinyalert::shinyalert("Long to wide transformation successful", type = "success")
+  }, error = function(e) {
+    shinyalert::shinyalert("Transformation Unsuccessful. Please reset data.", type = "error")
+  })
+})
+
+# 6. Wide to Long Transformation ----
 shiny::observeEvent(input$wide_long_trans, {
+  if (is.null(key_value_header$header) || is.null(variable_value_header$header)) {
+    shinyalert::shinyalert("Transformation Error", "Please perform long to wide transformation first.", type = "error")
+    shiny::validate(
+      shiny::need(key_value_header$header, "No key variable"),
+      shiny::need(variable_value_header$header, "No value variable")
+    )
+  }
 
-  # Stop if long data hasn't been transformed to wide data via the App
-    if (is.null(key_value_header$header) & is.null(variable_value_header$header)){
+  temp_wide_format <- shiny::isolate(App_data$values)
 
-      # Error notification
-      shinyalert::shinyalert(title = "Long data has not been transformed to wide data",
-                             text = "To transform back to wide data, please transform the long data to wide data first",
-                             type = "error")
-
-      # Validation
-      shiny::validate(shiny::need(key_value_header$header, "There is no key variable"),
-                      shiny::need(variable_value_header$header, "There is no value variable"))
-
-      }
-
-  # Un-formatted Data Storage
-  shiny::isolate({temp_wide_format <- App_data$values})
-
-  # Convert to character so that these values become options under the key value column header
-  key_value_options$data <- as.character(key_value_options$data)
-
-  # Convert wide data back to long data - generates error notification if transformation is unsuccessful
-  # For data transform back to long, column headers are re-ordered to the same order as before.
-  tryCatch(
-
-    # Checks for error - if no error, this then performs the transformation
-    expr = {App_data$values <- temp_wide_format |>
-      tidyr::gather_(key_value_header$header,variable_value_header$header,key_value_options$data) |>
+  tryCatch({
+    App_data$values <- temp_wide_format |>
+      tidyr::gather_(key_value_header$header, variable_value_header$header, key_value_options$data) |>
       dplyr::select(all_of(column_values$order))
 
-    # Success Notification
-    shinyalert::shinyalert(title = "Wide to long transform successful",
-                           type = "success")},
+    shinyalert::shinyalert("Wide to long transform successful", type = "success")
+  }, error = function(e) {
+    shinyalert::shinyalert("Transformation Unsuccessful. Please reset data.", type = "error")
+  })
 
-    # Error Notification
-      error = function(e)
-        shinyalert::shinyalert("Transformation Unsuccessful. Please reset data.",type = "error")
-      )
-
-  # Clear options for wide to long transform
   key_value_header$header <- NULL
   variable_value_header$header <- NULL
   key_value_options$data <- NULL
   column_values$order <- NULL
-  temp_wide_format <- NULL
-
-  })
+})
 
 # 7. Data Visualisation ----
-
 output$format_data <- DT::renderDataTable({
-
   cb <- htmlwidgets::JS('function(){debugger;HTMLWidgets.staticRender();}')
-  Format_Data <- sdcshinyapp::Table_Render(App_data$values,cb)
+  sdcshinyapp::Table_Render(App_data$values, cb)
+})
 
-  })
-
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-# End ----
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+# END OF SCRIPT ----
