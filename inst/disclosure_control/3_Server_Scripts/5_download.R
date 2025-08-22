f03b3d5f00da11fd08b05e2b675a2c2382d1fb95
rjmithcell92 <robertjames.mitchell@phs.scot>
2025-08-22 10:06:03 +0000

    Streamline Download Server code and generate shiny alert to indicate that training data reset won't work if there is no training data present.

--- inst/disclosure_control/3_Server_Scripts/5_download.R
+++ inst/disclosure_control/3_Server_Scripts/5_download.R
@@ -1,32 +1,23 @@
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-#Run on Posit version: 4.1.2
-#Last updated: 02 Oct 2024
-#By: Robert Mitchell
-#Script: 5_download.R
-#Purpose: To download data from app
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+# ------------------------------------------------------------------------------
+# Script Name : 5_download.R
+# Purpose     : Download data from app
+# Last Update : 22 Aug 2025
+# Author      : Robert Mitchell
+# Posit Version: 4.4.2
+# ------------------------------------------------------------------------------
 
 # 1. Data Visualisation ----
-
 output$Final_data <- DT::renderDataTable({
-
   cb <- htmlwidgets::JS('function(){debugger;HTMLWidgets.staticRender();}')
-  final <- sdcshinyapp::Table_Render(App_data$values,cb)
-
-  })
+  sdcshinyapp::Table_Render(App_data$values, cb)
+})
 
 # 2. Data Reset and Download ----
 
-## 1. Download and reset of uploaded data ----
-source("3_Server_Scripts/5-01_live.R",  local = TRUE)$value
+# Download and reset of uploaded data
+source("3_Server_Scripts/5-01_live.R", local = TRUE)$value
 
-## 2. Download and reset of training data ----
-source("3_Server_Scripts/5-02_train.R",  local = TRUE)$value
+# Download and reset of training data
+source("3_Server_Scripts/5-02_train.R", local = TRUE)$value
 
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-# End ----
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+# END OF SCRIPT ----
